d7bb2667c59ae93da41bbead4514b1dcd240e926
ahmedl3swy <ahmed.a.alesawy@gmail.com>
2024-03-10 22:55:10 +0200

    added AuthController

---
+++ src/application/Controllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using application.Models;
+using Microsoft.EntityFrameworkCore;
+namespace application.Controllers
+{
+    public class AuthController
+    {
+        iti_ExamContext db = new iti_ExamContext();
+        public User Login(string email, string password)
+        {
+          var user = db.Users
+         .Include(u => u.Student)
+         .Include(u => u.Instructor)
+         .FirstOrDefault(u => u.Email == email && u.Password == password);
+
+            if (user != null)
+            {
+                return user;
+            }
+            return null;
+        }
+        
+    }
+}
