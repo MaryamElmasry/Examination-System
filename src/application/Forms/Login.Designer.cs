c92498847531757f3de668e5d8319bed4a4393b1
ahmedl3swy <ahmed.a.alesawy@gmail.com>
2024-03-10 22:55:50 +0200

    added Login Form ui

--- src/application/Forms/Login.Designer.cs
+++ src/application/Forms/Login.Designer.cs
@@ -28,12 +28,108 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            picStudent = new PictureBox();
+            label1 = new Label();
+            txtEmail = new TextBox();
+            label2 = new Label();
+            txtPassword = new TextBox();
+            btnLogin = new Button();
+            ((System.ComponentModel.ISupportInitialize)picStudent).BeginInit();
+            SuspendLayout();
+            // 
+            // picStudent
+            // 
+            picStudent.Image = (Image)resources.GetObject("picStudent.Image");
+            picStudent.ImeMode = ImeMode.NoControl;
+            picStudent.Location = new Point(206, 12);
+            picStudent.Margin = new Padding(4, 3, 4, 3);
+            picStudent.Name = "picStudent";
+            picStudent.Size = new Size(116, 120);
+            picStudent.SizeMode = PictureBoxSizeMode.Zoom;
+            picStudent.TabIndex = 29;
+            picStudent.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F);
+            label1.Location = new Point(43, 206);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(76, 32);
+            label1.TabIndex = 31;
+            label1.Text = "Email:";
+            label1.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // txtEmail
+            // 
+            txtEmail.Location = new Point(205, 209);
+            txtEmail.Margin = new Padding(4, 5, 4, 5);
+            txtEmail.Name = "txtEmail";
+            txtEmail.Size = new Size(284, 31);
+            txtEmail.TabIndex = 30;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F);
+            label2.Location = new Point(44, 265);
+            label2.Margin = new Padding(4, 0, 4, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(116, 32);
+            label2.TabIndex = 33;
+            label2.Text = "Password:";
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(206, 268);
+            txtPassword.Margin = new Padding(4, 5, 4, 5);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(283, 31);
+            txtPassword.TabIndex = 32;
+            // 
+            // btnLogin
+            // 
+            btnLogin.BackColor = Color.Teal;
+            btnLogin.FlatAppearance.BorderSize = 0;
+            btnLogin.FlatStyle = FlatStyle.Flat;
+            btnLogin.Font = new Font("Segoe UI", 11F);
+            btnLogin.ForeColor = SystemColors.ButtonFace;
+            btnLogin.Location = new Point(191, 340);
+            btnLogin.Margin = new Padding(4, 5, 4, 5);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(197, 60);
+            btnLogin.TabIndex = 34;
+            btnLogin.Text = "Login";
+            btnLogin.UseVisualStyleBackColor = false;
+            btnLogin.Click += btnLogin_Click;
+            // 
+            // Login
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(525, 450);
+            Controls.Add(btnLogin);
+            Controls.Add(label2);
+            Controls.Add(txtPassword);
+            Controls.Add(label1);
+            Controls.Add(txtEmail);
+            Controls.Add(picStudent);
+            Name = "Login";
+            Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)picStudent).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private PictureBox picStudent;
+        private Label label1;
+        private TextBox txtEmail;
+        private Label label2;
+        private TextBox txtPassword;
+        private Button btnLogin;
     }
 }
