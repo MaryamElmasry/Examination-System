4e59e7844721911d9c77644cd2818f90a6cc6b68
AhmedL3swy <Ahmed.a.alesawy@gmail.com>
2024-03-19 13:26:51 +0200

    Added Generate Student per DeptID Report Logic

---
+++ src/application/Forms/Reports.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static CrystalReportGenerator.Program;
+
+namespace application.Forms
+{
+    public partial class ReportsForm : Form
+    {
+        public ReportsForm()
+        {
+            InitializeComponent();
+        }
+        private void btnSR_Click_1(object sender, EventArgs e)
+        {
+           
+            try
+            {
+                int DeptID = int.Parse(txtDeptID.Text);
+                ReportGenerator.DisplayDeptReport(DeptID);
+            }
+            catch 
+            {
+                MessageBox.Show("DeptID is Required and Must be Int Value");
+            }
+            
+        }
+    }
+
+}
